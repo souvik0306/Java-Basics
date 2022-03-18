43205bf511db1e473f8eda68ca175fa3920b65b1
Souvik Datta <souvikdatta123@gmail.com>
2022-03-18 23:05:43 +0530

    non primitive datatype

---
+++ non_primitive_datatypes.java
@@ -0,0 +1,20 @@
+public class non_primitive_datatypes
+{
+    public static void main(String[] args)
+    {
+    String str = "test";
+    System.out.println(str);
+
+    String str1 = new String("test2");
+    System.out.println(str1);
+
+    int arr[];
+    arr = new int[2];
+    arr[0] = 0;
+    arr[1] = 1;
+    // arr[2] = 2;
+
+    System.out.println(arr);
+    System.out.println(arr[0]);
+    }
+}
