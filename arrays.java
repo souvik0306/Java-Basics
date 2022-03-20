792a6682248bb0a2db12e86f11b2378e6b47424d
Souvik Datta <souvikdatta123@gmail.com>
2022-03-20 21:43:59 +0530

    arrays

---
+++ arrays.java
@@ -0,0 +1,10 @@
+public class arrays {
+    public static void  main(String[] args) {
+      int[] arrray; //declare an array of integers
+      arrray = new int[5]; //allocates the memory for 5 elements
+      arrray[1] = 3; //1 st element is 3
+
+      System.out.println(arrray[1]); //print 
+    }
+    
+}
--- data_types.java
+++ data_types.java
@@ -1,5 +1,11 @@
-public class data_types {
-    public static void main(String[] args)
+public class data_types {  //ensure that the class name and file name are the same
+    // everything in java is an object
+    // class is a blueprint for an object
+    // object can be defined as an instance of a class
+    public static void main(String args[]) 
+    // we need a main method to run a java program 
+    // we write static so as to run the program without creating an object of the class
+    // void is written to avoid any return 
     {
      double a = 50.2;
      System.out.println(a);
