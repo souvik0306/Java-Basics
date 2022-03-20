30ac57254c9ce2af6c2e3122d0d7698d368e98f8
Souvik Datta <souvikdatta123@gmail.com>
2022-03-20 23:16:32 +0530

    if else

---
+++ comparison_operators.java
@@ -0,0 +1,14 @@
+public class comparison_operators {
+    public static void main(String args[]){
+        int value1 = 5;
+        int value3 = 6;
+        if(value1==value3)
+        {
+            System.out.println("Same");
+        }
+        else
+        {
+            System.out.println("not same");
+        }
+    }
+}
