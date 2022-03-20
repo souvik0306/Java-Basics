0c9908acfa6afe723dc10274c0cd0305e3bbd7bd
Souvik Datta <souvikdatta123@gmail.com>
2022-03-20 22:00:08 +0530

    arithmetic

---
+++ arithmetic.java
@@ -0,0 +1,12 @@
+public class arithmetic {
+    public static void main(String args[]){
+        int a = 4;
+        int b = 5;
+        int c = a+b;
+        int d = b-a;
+        int e = a*b;
+        int f = b%a;
+        System.out.printf("%d %d %d %d",c,d,e,f);
+    }
+    
+}
