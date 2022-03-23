371b331beefa0d4ad83db8dcd0e82dcf7d3c7941
Souvik Datta <souvikdatta123@gmail.com>
2022-03-23 21:47:11 +0530

    comparison

---
+++ comparison.java
@@ -0,0 +1,16 @@
+public class comparison {
+    public static void main(String args[]){
+        int value1 = 1;
+        // int value3 = 1;
+        int value2 = 3;
+        if ((value1==1) && (value2==1))
+        {
+            System.out.println("Equal");
+        }
+        if ((value2==1) || (value1==1))
+        {
+            System.out.println("Not");
+        }
+        }
+    
+}
